6f1c0ebce00c84b9ac79bb01992949724789cfe3
PHILLIPE VALADARES DIAS PRCT00194154 <prct00194154@pbh.rmi>
2025-11-27 15:50:06 -0300

    melhorias

--- codigo-fonte/Login/Script.js
+++ codigo-fonte/Login/Script.js
@@ -1,70 +1,28 @@
 
-const container = document.querySelector('.container');
-const registerBtn = document.querySelector('.register-btn');
-const loginBtn = document.querySelector('.login-btn');
+const menuBtn = document.querySelector(".icon-btn");
+const nav = document.querySelector("nav");
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add('active');
+menuBtn.addEventListener("click", () => {
+    nav.classList.toggle("active"); // adiciona/remove classe
 });
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove('active');
-});
+const logo = document.querySelector(".site-logo");
+logo.style.cursor = "pointer";
 
-
-
-
-const registerForm = document.querySelector('.register form');
-const loginForm = document.querySelector('.login form');
-
-registerForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const username = registerForm.querySelector('input[placeholder="Usuario"]').value;
-    const email = registerForm.querySelector('input[placeholder="Email"]').value;
-    const password = registerForm.querySelector('input[placeholder="Senha"]').value;
-
-    const userData = {
-        username,
-        email,
-        password
-    };
-
-    
-    localStorage.setItem('usuario', JSON.stringify(userData));
-
-    alert("Usuário registrado com sucesso!");
-    container.classList.remove('active'); 
+logo.addEventListener("click", () => {
+    window.location.href = "index.html"; // página inicial
 });
 
 
+const loginBtn = document.querySelector(".login-btn");
+const registerBtn = document.querySelector(".register-btn");
+const container = document.querySelector(".container");
 
-
-loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const username = loginForm.querySelector('input[placeholder="Usuario"]').value;
-    const password = loginForm.querySelector('input[placeholder="Senha"]').value;
-
-    const savedUser = JSON.parse(localStorage.getItem('usuario'));
-
-    if (!savedUser) {
-        alert("Nenhum usuário registrado!");
-        return;
-    }
-
-    if (username === savedUser.username && password === savedUser.password) {
-        alert("Login realizado com sucesso!");
-        
-    } else {
-        alert("Usuário ou senha incorretos!");
-    }
+registerBtn.addEventListener("click", () => {
+    container.classList.add("active");
 });
 
+loginBtn.addEventListener("click", () => {
+    container.classList.remove("active");
+});
 
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navUL = document.querySelector('nav ul');
-
-    menuToggle.addEventListener('click', () => {
-        navUL.classList.toggle('active');
-    });
